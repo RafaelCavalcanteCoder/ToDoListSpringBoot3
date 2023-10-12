a544b0eb923fb080ec2ffe7061a231579f43e480
Rafael <rafa@gmail.com>
2023-10-11 22:15:17 -0300

    Início do TBL Task

--- src/main/java/br/com/sunrise/todolist/user/UserController.java
+++ src/main/java/br/com/sunrise/todolist/user/UserController.java
@@ -1,11 +1,14 @@
 package br.com.sunrise.todolist.user;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 @RestController
 @RequestMapping("users")
 public class UserController {
@@ -21,6 +24,10 @@ public class UserController {
         if(badRequest != null){
         return ResponseEntity.status(400).body("Usuário Já Existe!");
         }
+
+        var passwordHashCode = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashCode);
 
         var createRepository = iUserRepository.save(userModel);
         return ResponseEntity.status(200).body(createRepository);
